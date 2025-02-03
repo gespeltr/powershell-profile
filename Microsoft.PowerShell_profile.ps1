872db9a629cc5b057e437b59d2d63f98d3c1dcc8
gespeltr <guillemespeltr@gmail.com>
2025-02-03 16:38:45 +0100

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,22 @@
+function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
+
+function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
+
+function sysinfo { Get-ComputerInfo }
+
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function docs { 
+    $docs = if(([Environment]::GetFolderPath("MyDocuments"))) {([Environment]::GetFolderPath("MyDocuments"))} else {$HOME + "\Documents"}
+    Set-Location -Path $docs
+}
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/pure.omp.json" | Invoke-Expression
