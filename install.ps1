50920e09daf08a0f5d7185e8dca34144f46ab2e3
gespeltr <guillemespeltr@gmail.com>
2025-02-04 18:19:09 +0100

    fix winget install timeout

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 #Winget
 Write-Host "Installing Winget"
-Invoke-WebRequest -Uri "https://aka.ms/getwingetpreview" -OutFile "winget.msixbundle"
+Start-BitsTransfer -Source "https://aka.ms/getwinget" -Destination "winget.msixbundle"
 Add-AppxPackage -Path .\winget.msixbundle
 Remove-Item -Path .\winget.msixbundle
 
