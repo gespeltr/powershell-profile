9fdd0b8cc280c37fe7f745a336e73855950d89d5
gespeltr <guillemespeltr@gmail.com>
2025-02-04 18:21:38 +0100

    set PATH & install CascadiaCode fonts

--- install.ps1
+++ install.ps1
@@ -15,3 +15,12 @@ winget install --id Microsoft.PowerShell --source winget
 #Oh My Posh
 Write-Host "Installing Oh My Posh"
 winget install JanDeDobbeleer.OhMyPosh -s winget
+
+#Set PATH
+Write-Host "Set Oh My Posh PATH"
+$env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
+
+#Oh My Posh Fonts
+Write-Host "Installing CascadiaCode font"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") #Reload path
+oh-my-posh font install CascadiaCode
