fb9374135a281f8fb83c2be7848a474c676644a8
gespeltr <guillemespeltr@gmail.com>
2025-02-04 21:39:47 +0100

    add

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -29,6 +29,9 @@ Remove-Alias -Force -Name gc
 Remove-Alias -Force -Name gp
 Remove-Alias -Force -Name gl
 
+#Brave
+function b {& 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Brave.lnk' $pwd}
+
 & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\pure.omp.json" --print) -join "`n"))
 Import-Module Terminal-Icons
 Set-PSReadLineOption -PredictionViewStyle ListView
